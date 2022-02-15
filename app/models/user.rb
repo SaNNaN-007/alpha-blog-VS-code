0476f46174e0a9cbd97f4ac8c1b16a995be44a70
SaNNaN-007 <sannantariq0007@gmail.com>
2022-02-15 12:05:15 +0500

    show user in index article

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+
+    has_many :articles
+
+    before_save {self.email = email.downcase}
+
+    validates :username, presence: true, length: {minimum: 3, maximum: 30}, 
+    uniqueness: {case_sensitive: false}
+
+    VALID_EMAIL_REGEX = /\A(([A-Za-z0-9]*\.+*_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\+)|([A-Za-z0-9]+\+))*[A-Z‌​a-z0-9]+@{1}((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,4}\z/i
+    
+    validates :email, presence: true, length: {maximum: 100}, 
+    uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
+
+end

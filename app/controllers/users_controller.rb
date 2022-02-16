bfeb814a3103d82eb3767044a7909700e1457419
SaNNaN-007 <sannantariq0007@gmail.com>
2022-02-16 10:38:56 +0500

    add edit and show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
 
+    before_action :set_user, only: [:edit, :update, :show, :destroy]
+
     def new
         @user = User.new
     end
+
+    def show
+    end
     
     def create
-        @user = User.new(users_params)
+        @user = User.new(user_params)
         if @user.save
             flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
             redirect_to articles_path
@@ -14,10 +19,25 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit 
+    end
+
+    def update
+        if @user.update(user_params)
+            flash[:success] = "Your Account Updated Successfully."
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
 
     private
 
-    def users_params
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
         params.require(:user).permit(:username, :email, :password)
     end
 
